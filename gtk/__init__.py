b38e7c41b794942c1aab43b1fd8b0b6180fd083c
ap-- <mail@andreaspoehlmann.de>
2013-07-21 23:59:00 +0200

    remove the gtk stuff from apgooglelayer

--- gtk/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from _browser import GoogleDriveBrowser
-
-from _widgets import GoogleDriveWidget
-from _widgets import CairoLoadingWidget
-
-from _helpers import pil_from_url
-from _helpers import cairo_ImageSurface_from_pil
