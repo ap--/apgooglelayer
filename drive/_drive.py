6db50bc1584e015785fc58c06951b8645a365d70
ap-- <mail@andreaspoehlmann.de>
2013-07-27 14:24:29 +0200

    Untested filestructure speedup

--- drive/_drive.py
+++ drive/_drive.py
@@ -6,9 +6,10 @@ from _utils import SimpleTree
 
 class GoogleDriveIdHashableDict(dict):
     
-    def __init__(self, file_id, *args):
+    def __init__(self, file_id, *args, **kwargs):
         self.file_id = file_id
-        super(GoogleDriveIdHashableDict, self).__init__(*args)
+        self.file_name = kwargs.pop('title', '')
+        super(GoogleDriveIdHashableDict, self).__init__(*args, **kwargs)
 
     def __hash__(self):
         return hash(self.file_id)
@@ -81,6 +82,49 @@ class GoogleDrive(object):
         recurse('root', Tree)
         return Tree
 
+    def folder_structure2(self, http=None):
+        fields = ('items(title,id,mimeType,'
+                  'parents/id,parents/isRoot,labels/trashed)')
+        
+        DATA = []
+        for f in self.all_files(http=http, fields=fields):
+            if f.pop('labels')['trashed']: continue
+            DATA.append(f)
+        
+        # DATA: all files that are not deleted
+        # try to guess root_id:
+        GUESS = []
+        for d in DATA:
+            for p in d['parents']
+                if p['isRoot']:
+                    GUESS.append(p['id'])
+        GUESS = set(GUESS) # if there's only one possible solution, we're done.
+        if len(GUESS) == 1:
+            root_id, = GUESS
+        else:
+            root_id = self.about(http=http, 
+                    fields='rootFolderId').pop('rootFolderId')
+
+        # sort'em
+        def recurse(START, T):
+            # get all files, that have START as a parent
+            TRACK = []
+            N = len(DATA)
+            for i,d in enumerate(reversed(DATA)):
+                if any(p['id'] == START for p in d['parents']):
+                    TRACK.append(DATA.pop(N-i-1))
+            
+            for f in TRACK:
+                _id = f.pop('id')
+                f.pop('parents')
+                Hid = GoogleDriveIdHashableDict(_id, **f)
+                T[Hid] # creates entry through defaultdict
+                if 'folder' in f['mimeType']:
+                    recurse(START=_id, T=T[Hid])
+        
+        Tree = SimpleTree()
+        recurse(root_id, Tree)
+        return Tree
     
     def folder_structure_as_gtktreestore(self, http=None, fields='items(id,mimeType)'):
         tree = self.folder_structure(http=http, fields=fields)
