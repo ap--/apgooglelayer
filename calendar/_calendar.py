2b8e044e1ec8e120ddc4cc2aefdb78e437c8b605
ap-- <mail@andreaspoehlmann.de>
2013-07-27 14:23:18 +0200

    added some calendar functionality

--- calendar/_calendar.py
+++ calendar/_calendar.py
@@ -2,12 +2,22 @@
 
 class GoogleCalendar(object):
 
-    def __init__(self, service, name):
+    def __init__(self, service, name=None):
         self.service = service
         self.calendarName = name
-        self.calendarId = self.getcalendarid()
+        if name is not None:
+            self.calendarId = self.getcalendarid()
+        else:
+            self.calendarId = None
 
-    def _findcalendarid(self):
+    def _findcalendarid(self, name=None):
+        if name is not None:
+            pass
+        elif self.calendarName is not None:
+            name = self.calendarName
+        else:
+            raise ValueError('provide name, or set calendarName')
+        
         page_token=None
         while True:
             clist = self.service.calendarList().list(pageToken=page_token).execute()
@@ -38,11 +48,14 @@ class GoogleCalendar(object):
         return _id
 
 
-    def iterevents(self):
+    def iterevents(self, **x):
+        http = x.pop('http', None)
+        if self.calendarId is None:
+            raise ValueError('please set calendarId')
         page_token = None
         while True:
             events = self.service.events().list(calendarId=self.calendarId,
-                                                pageToken=page_token).execute()
+                                    pageToken=page_token, **x).execute(http=http)
             try:
                 events['items']
             except KeyError:
@@ -56,6 +69,8 @@ class GoogleCalendar(object):
 
 
     def addevent(self, date, summary, description, location, duration):
+        if self.calendarId is None:
+            raise ValueError('please set calendarId')
         start = date
         end = start + duration
         start = start.isoformat('T')
@@ -74,12 +89,26 @@ class GoogleCalendar(object):
                                      body=event).execute()
 
     def addrawevent(self, event):
+        if self.calendarId is None:
+            raise ValueError('please set calendarId')
         self.service.events().insert(calendarId=self.calendarId,
                                      body=event).execute()
 
     def deleteevent(self, eventid):
+        if self.calendarId is None:
+            raise ValueError('please set calendarId')
         self.service.events().delete(calendarId=self.calendarId,
                                     eventId=eventid).execute()
 
-
+    
+    def get_events_from_calendar(self, calendarId, http):
+        old = self.calendarId
+        self.calendarId = calendarId
+        EVENTS={}
+        for ev in self.iterevents(http=http,
+                fields="items(id,start,recurrence,location,summary,description)"):
+            _id = ev.pop('id')
+            EVENTS[_id] = ev
+        self.calendarId = old
+        return EVENTS
 
